560d255df67df667d9da1b706b832b9528dbe82f
Evan Lynch <evan.f.lynch@gmail.com>
2015-05-21 11:53:02 -0400

    Added threading

--- chain.site/chain.site.c
+++ chain.site/chain.site.c
@@ -14,13 +14,23 @@
 typedef struct chain_site
 {
     t_object s_obj;
-    long s_value;
+    t_systhread s_systhread;
+    t_systhread_mutex s_mutex;
+    int s_systhread_cancel;
+    void *s_qelem;
+    void *s_outlet;
+    int s_sleeptime;
+    int s_value;
 } t_chain_site;
 
 void *chain_site_new(t_symbol *s, long argc, t_atom *argv);
 void chain_site_free(t_chain_site *x);
 void chain_site_int(t_chain_site *x, long n);
 void chain_site_bang(t_chain_site *x);
+void chain_site_sleeptime(t_chain_site *x, long sleeptime);
+void chain_site_stop(t_chain_site *x);
+void *chain_site_threadproc(t_chain_site *x);
+void chain_site_qfn(t_chain_site *x);
 
 static t_class *s_chain_site_class = NULL;
 
@@ -32,6 +42,8 @@ int C74_EXPORT main(void)
     
     class_addmethod(c, (method)chain_site_bang, "bang", 0);
     class_addmethod(c, (method)chain_site_int, "int", A_LONG, 0);
+    class_addmethod(c, (method)chain_site_sleeptime, "sleeptime", A_DEFLONG, 0);
+    class_addmethod(c, (method)chain_site_stop, "cancel", 0);
     
     class_register(CLASS_BOX, c);
     
@@ -44,6 +56,11 @@ void *chain_site_new(t_symbol *s, long argc, t_atom *argv)
 {
     t_chain_site *x = (t_chain_site *)object_alloc(s_chain_site_class);
     
+    x->s_outlet = outlet_new(x, NULL);
+    x->s_qelem = qelem_new(x, (method)chain_site_qfn);
+    x->s_systhread = NULL;
+    systhread_mutex_new(&x->s_mutex,0);
+    x->s_sleeptime = 1000;
     x->s_value = 0;
     
     return x;
@@ -51,14 +68,80 @@ void *chain_site_new(t_symbol *s, long argc, t_atom *argv)
 
 void chain_site_free(t_chain_site *x)
 {
+    chain_site_stop(x);
+
+    if (x->s_qelem)
+        qelem_free(x->s_qelem);
+
+    if (x->s_mutex)
+        systhread_mutex_free(x->s_mutex);
+}
+
+void chain_site_sleeptime(t_chain_site *x, long sleeptime)
+{
+    if (sleeptime<10)
+        sleeptime = 10;
+    x->s_sleeptime = sleeptime;
 }
 
 void chain_site_int(t_chain_site *x, long n)
 {
-    x->s_value = n;
+    systhread_mutex_lock(x->s_mutex);
+    x->s_value = (int)n;
+    systhread_mutex_unlock(x->s_mutex);
 }
 
 void chain_site_bang(t_chain_site *x)
 {
-    post("value is %ld", x->s_value);
+    chain_site_stop(x);
+
+    if (x->s_systhread == NULL) {
+        post("Starting a new thread");
+        systhread_create((method) chain_site_threadproc, x, 0, 0, 0, &x->s_systhread);
+    }
+
+}
+
+void chain_site_stop(t_chain_site *x)
+{
+    unsigned int ret;
+
+    if (x->s_systhread) {
+        post("stopping our thread");
+        x->s_systhread_cancel = true;
+        systhread_join(x->s_systhread, &ret);
+        x->s_systhread = NULL;
+    }
+}
+
+void chain_site_qfn(t_chain_site *x){
+
+    int myValue;
+
+    systhread_mutex_lock(x->s_mutex);
+    myValue = x->s_value;
+    systhread_mutex_unlock(x->s_mutex);
+
+    outlet_int(x->s_outlet, myValue);
+}
+
+void *chain_site_threadproc(t_chain_site *x)
+{
+    while(1){
+        if (x->s_systhread_cancel)
+            break;
+
+        systhread_mutex_lock(x->s_mutex);
+        x->s_value++;
+        systhread_mutex_unlock(x->s_mutex);
+
+        qelem_set(x->s_qelem);
+
+        systhread_sleep(x->s_sleeptime);
+    }
+
+    x->s_systhread_cancel = false;
+
+    systhread_exit(0);
+    return NULL;
 }
